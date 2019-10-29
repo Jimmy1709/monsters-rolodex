f5530f559f110f2e1a688f755ba80beed0522a75
Utkarsh Tripathi <utkarshtripathi1705@gmail.com>
2019-10-29 22:21:25 +0530

    Updates

---
+++ precache-manifest.60dca78ef9f99b8e494454258f16eff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb06871abd9d90f1837c082c0d0d039",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c38e46b17b8881f1c30e",
+    "url": "/monsters-rolodex/static/css/main.9024f0cd.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "c38e46b17b8881f1c30e",
+    "url": "/monsters-rolodex/static/js/main.4a59d4d9.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
