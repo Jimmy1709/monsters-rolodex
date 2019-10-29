b5ba23aa0483500a1f1c8f4d11bf9e129a44fc37
Utkarsh Tripathi <utkarshtripathi1705@gmail.com>
2019-10-29 22:36:57 +0530

    message for Red

--- src/App.js
+++ src/App.js
@@ -38,7 +38,10 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <h1> Monster Rolodex</h1>
+        <h1>  Amrita I just Love you So much.</h1>
+        <h2> It has been a year since i met you first. </h2>  
+        <h2> I want to tell you that it is a very important and special day for me </h2> 
+        <h1> I promise you a very happy and happening ;) life ahead. I LOVE YOU !!</h1>
         <SearchBox
             placeholder='search monsters'
             handleChange= {this.handleChange}
